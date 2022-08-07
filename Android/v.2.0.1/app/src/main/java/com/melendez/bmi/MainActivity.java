280b10cb732041034081380f7dd1c26f86eadf7e
Mark Melendez <MarkMelendez2008@outlook.com>
2022-08-07 12:54:05 +0800

    水平划分完成

--- Android/v.2.0.1/app/src/main/java/com/melendez/bmi/MainActivity.java
+++ Android/v.2.0.1/app/src/main/java/com/melendez/bmi/MainActivity.java
@@ -1,14 +1,69 @@
 package com.melendez.bmi;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.lsp.RulerView;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "Melendez";
+    private RulerView Height_Ruler;
+    private RulerView Weight_Ruler;
+    private TextView Show_Text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //查找控件
+        Height_Ruler = findViewById(R.id.Height_Ruler);
+        Weight_Ruler = findViewById(R.id.Weight_Ruler);
+        Show_Text = findViewById(R.id.Show_Text);
+        Log.e(TAG, "onCreate: 控件查找完成");
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void Get_Calculation(View view) {
+        //RulerView返回值读取
+        float Height = Height_Ruler.currentScale;
+        float Weight = Weight_Ruler.currentScale;
+        //计算
+        float bmi = Weight / (Height * Height);
+        //保留两位小数
+        BigDecimal bd = new BigDecimal(bmi);
+        bmi = (float) bd.setScale(2, RoundingMode.HALF_UP).
+
+                doubleValue();
+        if (bmi > 100) {
+            Show_Text.setText("请输入正确的身高体重！！！");
+            Log.e(TAG, "get_calculation: 身高过低/体重过大");
+        }
+        //身体水平判断
+        else {
+            //水平划分
+            String level;
+            if (bmi < 19) {
+                level = getString(R.string.Thin);
+            } else if (19 <= bmi && bmi < 25) {
+                level = getString(R.string.Normal);
+            } else if (25 <= bmi && bmi < 30) {
+                level = getString(R.string.Overweight);
+            } else if (30 <= bmi && bmi < 39) {
+                level = getString(R.string.Seriously_overweight);
+            } else {
+                level = getString(R.string.Extremely_overweight);
+            }
+            //在前端输出
+            Show_Text.setText(getString(R.string.Yours_Bmi) + bmi + getString(R.string.In) + level + getString(R.string.Stage));
+        }
     }
 }
